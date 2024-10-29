689297a6ed2a29f7a03900953eaa5b1ae879e787
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-10-29 17:30:31 -0400

    print word count

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        wc_list = file_contents.split()
+        print( len(wc_list) )
 
 main()
